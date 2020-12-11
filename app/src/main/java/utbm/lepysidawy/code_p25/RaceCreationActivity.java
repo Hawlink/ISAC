c86cb78db2aa2504b78d47fcc6252dfbc7565553
Nicolas Lépy <33573107+nicolasLepy@users.noreply.github.com>
2020-12-11 15:46:22 +0100

    Players repartition algorithm implemented

--- app/src/main/java/utbm/lepysidawy/code_p25/RaceCreationActivity.java
+++ app/src/main/java/utbm/lepysidawy/code_p25/RaceCreationActivity.java
@@ -15,6 +15,7 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -150,6 +151,45 @@ public class RaceCreationActivity extends AppCompatActivity {
     }
 
     /**
+     * Copy teams ArrayList in a new ArrayList
+     */
+    private ArrayList<ArrayList<Runner>> copyTeams(ArrayList<ArrayList<Runner>> teams)
+    {
+        ArrayList<ArrayList<Runner>> res = new ArrayList<ArrayList<Runner>>();
+
+        for(int i = 0; i<teams.size(); i++){
+            res.add(new ArrayList<Runner>(teams.get(i)));
+        }
+        return res;
+    }
+
+    /**
+     * Calculate level variance between teams
+     */
+    private double variance(ArrayList<ArrayList<Runner>> teams){
+        ArrayList<Double> averages = new ArrayList<>();
+        for(ArrayList<Runner> team : teams){
+            int sum = 0;
+            for(Runner runner : team){
+                sum += runner.getNiveau();
+            }
+            averages.add(sum/(team.size() + 0.0));
+        }
+
+        float average = 0;
+        for(double f : averages){
+            average += f;
+        }
+        average /= (averages.size() + 0.0f);
+
+        double deviation = 0;
+        for(double f : averages){
+            deviation += ((f - average) * (f-average));
+        }
+        return Math.sqrt((1/(teams.size() + 0.0f))*deviation);
+    }
+
+    /**
      * Method used to create the different teams
      */
     public void createTeams(int raceId) {
@@ -164,6 +204,21 @@ public class RaceCreationActivity extends AppCompatActivity {
                 this.participantsList.remove(selected);
             }
         }
+
+        Random random = new Random();
+        //Loop and try to minimise variance between teams by randomly swapping two players
+        for(int i = 0; i<2000;i++){
+            ArrayList<ArrayList<Runner>> test = copyTeams(teams);
+            int swapTeam1 = random.nextInt(nbTeams);
+            int swapPlayer1 = random.nextInt(3);
+            int swapTeam2 = random.nextInt(nbTeams);
+            int swapPlayer2 = random.nextInt(3);
+            Runner save = test.get(swapTeam1).get(swapPlayer1);
+            test.get(swapTeam1).set(swapPlayer1, test.get(swapTeam2).get(swapPlayer2));
+            test.get(swapTeam2).set(swapPlayer2, save);
+            if(variance(test) < variance(teams)) teams = test;
+        }
+
         for(int i = 0; i < teams.size(); i++) {
             for(int j = 0; j < 3; j++) {
                 ParticipateRace participation = new ParticipateRace(raceId, teams.get(i).get(j).getIdPlayer(), i+1, j+1);
